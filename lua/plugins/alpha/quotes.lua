5ac3687840ba85294ed4ed1bffb1a0122d35ac43
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-10-20 22:24:40 +0300

    setup alpha dashboard

---
+++ lua/plugins/alpha/quotes.lua
@@ -0,0 +1,66 @@
+local M = {}
+
+M.a = {
+	[[Simplicity, patience, compassion.]],
+	[[These three are your greatest treasures.]],
+	[[Simple in actions and thoughts, you return to the source of being.]],
+	[[Patient with both friends and enemies,]],
+	[[you accord with the way things are.]],
+	[[Compassionate toward yourself,]],
+	[[you reconcile all beings in the world.]],
+}
+
+M.b = {
+	[[Knowing others is intelligence;]],
+	[[knowing yourself is true wisdom.]],
+	[[Mastering others is strength; ]],
+	[[mastering yourself is true power.]],
+}
+
+M.c = {
+	[[When you are content to be simply yourself and don't compare or compete, everyone will respect you.]],
+}
+
+M.d = {
+	[[The truth is not always beautiful, nor beautiful words the truth.]],
+}
+
+M.e = {
+	[[If you understand others you are smart.]],
+	[[If you understand yourself you are illuminated.]],
+	[[If you overcome others you are powerful.]],
+	[[If you overcome yourself you have strength.]],
+	[[If you know how to be satisfied you are rich.]],
+	[[If you can act with vigor, you have a will.]],
+	[[If you don't lose your objectives you can be long-lasting.]],
+	[[If you die without loss, you are eternal.]],
+}
+
+M.f = {
+	[[The flame that burns Twice as bright burns half as long.]],
+}
+
+M.g = {
+	[[If you try to change it, you will ruin it. Try to hold it, and you will lose it.]],
+}
+
+M.h = {
+	[[Give evil nothing to oppose]],
+	[[and it will disappear by itself.]],
+}
+
+M.i = {
+	[[To understand the limitation of things, desire them.]],
+}
+
+M.j = {
+	[[Close your mouth,]],
+	[[block off your senses,]],
+	[[blunt your sharpness,]],
+	[[untie your knots,]],
+	[[soften your glare,]],
+	[[settle your dust.]],
+	[[This is the primal identity.]],
+}
+
+return M
