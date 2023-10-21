b6d6a61188253532ba635b80592a75da2534ffe3
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-10-21 12:50:02 +0300

    add more quotes

--- lua/plugins/alpha/quotes.lua
+++ lua/plugins/alpha/quotes.lua
@@ -18,11 +18,14 @@ M.b = {
 }
 
 M.c = {
-	[[When you are content to be simply yourself and don't compare or compete, everyone will respect you.]],
+	[[When you are content to be simply yourself]],
+	[[ and don't compare or compete,]],
+	[[ everyone will respect you.]],
 }
 
 M.d = {
-	[[The truth is not always beautiful, nor beautiful words the truth.]],
+	[[The truth is not always beautiful,]],
+	[[ nor beautiful words the truth.]],
 }
 
 M.e = {
@@ -37,11 +40,14 @@ M.e = {
 }
 
 M.f = {
-	[[The flame that burns Twice as bright burns half as long.]],
+	[[The flame that ]],
+	[[burns Twice as bright]],
+	[[burns half as long.]],
 }
 
 M.g = {
-	[[If you try to change it, you will ruin it. Try to hold it, and you will lose it.]],
+	[[If you try to change it, you will ruin it.]],
+	[[Try to hold it, and you will lose it.]],
 }
 
 M.h = {
@@ -50,7 +56,8 @@ M.h = {
 }
 
 M.i = {
-	[[To understand the limitation of things, desire them.]],
+	[[To understand the limitation of things,]],
+	[[ desire them.]],
 }
 
 M.j = {
@@ -61,6 +68,110 @@ M.j = {
 	[[soften your glare,]],
 	[[settle your dust.]],
 	[[This is the primal identity.]],
+}
+
+M.k = {
+	[[When people see some things as beautiful,]],
+	[[other things become ugly.]],
+	[[When people see some things as good,]],
+	[[other things become bad.]],
+}
+
+M.l = {
+	[[When there is no desire,]],
+	[[all things are at peace.]],
+}
+
+M.m = {
+	[[Trying to understand is like straining through muddy water. ]],
+	[[Have the patience to wait! Be still and allow the mud to settle.]],
+}
+
+M.n = {
+	[[Love is a decision]],
+	[[not an emotion!.]],
+}
+
+M.o = {
+	[[True words aren't eloquent;]],
+	[[eloquent words aren't true.]],
+	[[Wise men don't need to prove their point;]],
+	[[men who need to prove their point aren't wise.]],
+	[[]],
+	[[The Master has no possessions.]],
+	[[The more he does for others,]],
+	[[the happier he is.]],
+	[[The more he gives to others,]],
+	[[the wealthier he is.]],
+}
+
+M.p = {
+	[[Countless words ]],
+	[[count less ]],
+	[[than the silent balance ]],
+	[[between yin and yang.]],
+}
+
+M.q = {
+	[[Fill your bowl to the brim and it will spill. ]],
+	[[Keep sharpening your knife and it will blunt.]],
+}
+
+M.r = {
+	[[When nothing is done, ]],
+	[[nothing is left undone.]],
+}
+
+M.s = {
+	[[Not-knowing is true knowledge.]],
+	[[Presuming to know is a disease.]],
+	[[First realize that you are sick;]],
+	[[then you can move toward health.]],
+}
+
+M.t = {
+	[[What is firmly rooted]],
+	[[cannot be pulled out.]],
+}
+
+M.u = {
+	[[Knowing others is wisdom;]],
+	[[Knowing the self is enlightenment.]],
+	[[Mastering others requires force;]],
+	[[Mastering the self requires strength;]],
+	[[He who knows he has enough is rich.]],
+	[[Perseverance is a sign of will power.]],
+}
+
+M.v = {
+	[[He is free from self-display,]],
+	[[and therefore he shines;]],
+}
+
+M.w = {
+	[[The Wise Man is square but not sharp,]],
+	[[honest but not not malign,]],
+	[[straight but not severe,]],
+	[[bright but not dazzling.]],
+}
+
+M.x = {
+	[[The reason why heaven and earth are able to endure and continue thus long]],
+	[[is because they do not live of,]],
+	[[or for,]],
+	[[themselves.]],
+}
+
+M.y = {
+	[[Must you value what others value,]],
+	[[avoid what others avoid?]],
+	[[How ridiculous!.]],
+}
+
+M.z = {
+	[[The moral man does something,]],
+	[[and when no one responds]],
+	[[he rolls up his sleeves and uses force.]],
 }
 
 return M
