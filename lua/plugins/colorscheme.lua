e0477aec188f4ff789e524b6f205bd39d75c4dde
Ioan <latrovalievioan@gmail.com>
2023-10-24 14:44:38 +0300

    tweek colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,7 +11,7 @@ return {
 			-- or leave it empty to use the default settings
 			style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
 			light_style = "day", -- The theme is used when the background is set to light
-			transparent = false, -- Enable this to disable setting the background color
+			transparent = true, -- Enable this to disable setting the background color
 			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
 			styles = {
 				-- Style to be applied to different syntax groups
@@ -21,8 +21,8 @@ return {
 				functions = {},
 				variables = {},
 				-- Background styles. Can be "dark", "transparent" or "normal"
-				sidebars = "dark", -- style for sidebars, see below
-				floats = "dark", -- style for floating windows
+				sidebars = "transparent", -- style for sidebars, see below
+				floats = "transparent", -- style for floating windows
 			},
 			sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
 			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
@@ -33,7 +33,9 @@ return {
 			--- You can override specific color groups to use other groups or a hex color
 			--- function will be called with a ColorScheme table
 			---@param colors ColorScheme
-			on_colors = function(colors) end,
+			on_colors = function(colors)
+				colors.border = "#82aaff"
+			end,
 
 			--- You can override specific highlights to use other groups or a hex color
 			--- function will be called with a Highlights and ColorScheme table
