5e64479a4605566eb179547a4c8dc71bafd5d1c9
Ioan <latrovalievioan@gmail.com>
2024-03-05 09:35:47 +0200

    set tabwidth to 4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,9 @@
 local opt = vim.opt
 
 -- Tab / Indentation
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
 opt.expandtab = true
 opt.smartindent = true
 opt.wrap = false
