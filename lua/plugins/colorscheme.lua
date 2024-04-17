5d2eefeb166242f3c3af0bcb3465957074ba3680
Ioan Latrovaliev <ioan.latrovaliev@lucidlink.com>
2024-04-17 19:35:40 +0300

    fuck lspsaga and change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,23 +2,23 @@
 
 return {
 	{
-		'sainnhe/gruvbox-material',
-		name = 'gruvbox-material',
-        lazy = false,
+		"sainnhe/gruvbox-material",
+		name = "gruvbox-material",
+		lazy = false,
 		config = function()
 			vim.g.gruvbox_material_better_performance = 1
 			-- Fonts
 			vim.g.gruvbox_material_disable_italic_comment = 0
 			vim.g.gruvbox_material_enable_italic = 0
-			vim.g.gruvbox_material_enable_bold = 0
-			vim.g.gruvbox_material_transparent_background = 1
+			vim.g.gruvbox_material_enable_bold = 1
+			vim.g.gruvbox_material_transparent_background = 0
 			-- Themes
-			vim.g.gruvbox_material_foreground = 'medium'
-			vim.g.gruvbox_material_background = 'medium'
-			vim.g.gruvbox_material_ui_contrast = 'high' -- The contrast of line numbers, indent lines, etc.
-			vim.g.gruvbox_material_float_style = 'dim' -- Background of floating windows
+			vim.g.gruvbox_material_foreground = "medium"
+			vim.g.gruvbox_material_background = "medium"
+			vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+			vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
 
-			vim.cmd('colorscheme gruvbox-material')
-		end
+			vim.cmd("colorscheme gruvbox-material")
+		end,
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,16 @@
 -- FZF
-
-local mapkey = vim.keymap
-
 local config = function()
 	local telescope = require("telescope")
 	telescope.setup({
-    defaults = {
-        layout_strategy = 'vertical',
-        layout_config = {
-            height = 0.99,
-            width = 0.99,
-            preview_cutoff = 1
-        }
-    },
-})
+		defaults = {
+			layout_strategy = "vertical",
+			layout_config = {
+				height = 0.99,
+				width = 0.99,
+				preview_cutoff = 1,
+			},
+		},
+	})
 end
 
 return {
@@ -23,9 +20,9 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = config,
 	keys = {
-		mapkey.set("n", "<leader>ff", ":Telescope find_files<CR>"),
-		mapkey.set("n", "<leader>f", ":Telescope git_files<CR>"),
-		mapkey.set("n", "<leader>st", ":Telescope live_grep<CR>"),
-		mapkey.set("n", "<leader>fb", ":Telescope buffers<CR>"),
+		vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
+		vim.keymap.set("n", "<leader>f", ":Telescope git_files<CR>"),
+		vim.keymap.set("n", "<leader>st", ":Telescope live_grep<CR>"),
+		vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
 	},
 }
