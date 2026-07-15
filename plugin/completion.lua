ee957d18178e7a0628928cc94ccad780c1850ace
Ioan Latrovaliev <ioan.latrovaliev@lucidlink.com>
2026-07-15 18:43:54 +0300

    sss

--- plugin/completion.lua
+++ plugin/completion.lua
@@ -1,10 +1,55 @@
-vim.opt.completeopt = "menuone,noselect,popup"
-
--- Enable built-in LSP completion for each LSP client
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd('PackChanged', {
     callback = function(ev)
-        vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
-            autotrigger = true,
-        })
-    end,
+        if ev.data.spec.name == 'LuaSnip' and ev.data.kind ~= 'delete' then
+            if not ev.data.active then vim.cmd.packadd('LuaSnip') end
+            vim.fn.system({ 'make', '-C', vim.fn.stdpath('data') .. '/site/pack/core/opt/LuaSnip', 'install_jsregexp' })
+        end
+    end
+})
+
+vim.pack.add({
+    'https://github.com/onsails/lspkind.nvim',
+    'https://github.com/hrsh7th/cmp-nvim-lsp',
+    'https://github.com/hrsh7th/cmp-buffer',
+    'https://github.com/hrsh7th/cmp-path',
+    { src = 'https://github.com/L3MON4D3/LuaSnip', version = vim.version.range('2.x') },
+    'https://github.com/saadparwaiz1/cmp_luasnip',
+    'https://github.com/hrsh7th/nvim-cmp',
+})
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local lspkind = require("lspkind")
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+    }),
+    formatting = {
+        format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+        }),
+    },
+    window = {
+        documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered({
+            winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+        }),
+    },
 })
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,3 +1,9 @@
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+vim.lsp.config("*", {
+    capabilities = capabilities,
+})
+
 vim.lsp.enable({
     "eslint",
     "lua_ls",
